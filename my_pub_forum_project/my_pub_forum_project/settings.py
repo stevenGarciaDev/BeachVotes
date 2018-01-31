a322dfc4dbb7867a29aeff306261e9385d966a8e
stevenGarciaDev <stevgarcia2015@gmail.com>
2018-01-31 14:45:08 -0800

    Add index page to view and configure path to template and static directory

--- my_pub_forum_project/my_pub_forum_project/settings.py
+++ my_pub_forum_project/my_pub_forum_project/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'my_pub_forum_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
