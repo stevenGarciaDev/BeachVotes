ab926363fb1b47b0b608ef50f221ec85ecb3e262
stevenGarciaDev <stevgarcia2015@gmail.com>
2018-03-30 19:31:46 -0700

    basic htmlml files add

--- beach_votes_project/poll/urls.py
+++ beach_votes_project/poll/urls.py
@@ -3,4 +3,7 @@ from poll import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^create_poll', views.create_poll, name='create_poll'),
+    url(r'^my_profile', views.my_profile, name='my_profile'),
+    url(r'^show_polls', views.show_polls, name='show_polls')
 ]
--- beach_votes_project/poll/views.py
+++ beach_votes_project/poll/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 #Combines a template with HttpResponse and returns rendered text via web
 def index(request):
     return render(request, 'poll/index.html', context=None)
+
+def create_poll(request):
+    return HttpResponse("create a poll")
+
+def my_profile(request):
+    return HttpResponse("my profile")
+
+def show_polls(request):
+    return HttpResponse("show polls")
