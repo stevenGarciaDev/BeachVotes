cb20b5b333b854e0d520f0c38be2fdc03abded30
stevenGarciaDev <stevgarcia2015@gmail.com>
2018-04-01 10:08:31 -0700

    Make html templates use inheritance to reduce duplication

--- beach_votes_project/poll/views.py
+++ beach_votes_project/poll/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from poll.models import *
 
 #Combines a template with HttpResponse and returns rendered text via web
 def index(request):
     return render(request, 'poll/index.html', context=None)
 
 def create_poll(request):
-    return HttpResponse("create a poll")
+    return render(request, 'poll/create_poll.html', {})
 
 def my_profile(request):
-    return HttpResponse("my profile")
+    return render(request, 'poll/my_profile.html', {})
 
 def show_polls(request):
-    return HttpResponse("show polls")
+    return render(request, 'poll/show_polls.html', {})
