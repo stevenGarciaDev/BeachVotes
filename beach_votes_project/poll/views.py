7e091e143e217ec426c2aae64fdc23aa3a4b8756
stevenGarciaDev <stevgarcia2015@gmail.com>
2018-04-02 16:10:34 -0700

    Add view functionality for sign_up to enable creating a new user

--- beach_votes_project/poll/views.py
+++ beach_votes_project/poll/views.py
@@ -5,6 +5,24 @@ from poll.forms import *
 
 #Combines a template with HttpResponse and returns rendered text via web
 def index(request):
+    return render(request, 'poll/index.html', {})
+
+def create_poll(request):
+    return render(request, 'poll/create_poll.html', {})
+
+def my_profile(request):
+    return render(request, 'poll/my_profile.html', {})
+
+def show_polls(request):
+    return render(request, 'poll/show_polls.html', {})
+
+def view_poll(request):
+    return render(request, 'poll/view_poll.html', {})
+
+def login(request):
+    return render(request, 'poll/login.html', {})
+
+def sign_up(request):
     registered = False
 
     if request.method == 'POST':
@@ -29,26 +47,8 @@ def index(request):
         user_form = UserForm()
         user_profile_form = UserProfileForm()
 
-    return render(request, 'poll/index.html',
+    return render(request, 'poll/sign_up.html',
         context = {
             'user_form': user_form,
             'user_profile_form': user_profile_form,
             'registered': registered})
-
-def create_poll(request):
-    return render(request, 'poll/create_poll.html', {})
-
-def my_profile(request):
-    return render(request, 'poll/my_profile.html', {})
-
-def show_polls(request):
-    return render(request, 'poll/show_polls.html', {})
-
-def view_poll(request):
-    return render(request, 'poll/view_poll.html', {})
-
-def login(request):
-    return render(request, 'poll/login.html', {})
-
-def sign_up(request):
-    return render(request, 'poll/sign_up.html', {})
