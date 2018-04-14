749f1f232e9d95c6d3031982bf2201be84626756
Eric <eric.lee0051@gmail.com>
2018-04-14 16:44:54 -0700

    Adds admin page
    Categories, poll answer choices, and votes do not currently work/have databases in the admin page.
    
    I'm assuming its because there are no users or polls to represent them currently.
    
    To add tables
    python manage.py makemigrations
    python manage.py migrate
    
    python manage.py migrate --run-syncdb

--- beach_votes_project/poll/admin.py
+++ beach_votes_project/poll/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from poll.models import Category, User, UserProfile, Poll, PollAnswerChoice, Vote
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Poll)
+admin.site.register(PollAnswerChoice)
+admin.site.register(Vote)
