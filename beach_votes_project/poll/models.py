9cdde3c4a9b22b805891cd7d2860cce7d6374f8f
Eric <eric.lee0051@gmail.com>
2018-04-14 13:53:56 -0700

    profile

--- beach_votes_project/poll/models.py
+++ beach_votes_project/poll/models.py
@@ -36,6 +36,8 @@ class User(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    picture = models.ImageField(upload_to='profile_images', blank = True)
+
     def __str__(self):
         return self.user.username
 
